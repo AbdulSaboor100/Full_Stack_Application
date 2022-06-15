24709ffa3f6c77cfd113391c3116ec63553c1c7f
AbdulSaboor100 <70238888+AbdulSaboor100@users.noreply.github.com>
2022-06-15 08:20:31 -0700

    Created more apis

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,25 @@
 import express from "express";
+import Profile from "../../models/Profile.js";
+import User from "../../models/User.js";
+import authController from "../../middleware/authController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("profile route");
+router.post("/me", authController, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ message: "There is not profile for this user" });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 export default router;
